9af349dbe39725b3897ac5b4e1aa18370fcde6bc
michel ribeiro <michelribeiro2004@gmail.com>
2018-03-20 00:04:18 -0300

    news

--- src/App.js
+++ src/App.js
@@ -1,21 +1,129 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Grid, Row, FormGroup} from 'react-bootstrap';
+import list from './list'
 import './App.css';
 
+function isSearched(searchTerm){
+  return function(item) {
+    return !searchTerm || item.title.includes(searchTerm);
+  }
+}
+
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      list,
+      searchTerm: ''
+    }
+
+    this.removeItem = this.removeItem.bind(this);
+    this.searchValue = this.searchValue.bind(this);
+  }
+
+  removeItem(id) {
+    const updatedList = this.state.list.filter(item => item.objectId !== id);
+    this.setState({list: updatedList});
+  }
+
+  searchValue(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
+    
+    const { list, searchTerm } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Grid fluid>
+        <Row>
+          <div className="jumbotron">
+            <Search
+              onChange={this.searchValue}
+              value={searchTerm}> Search here
+            </Search>
+          </div>
+        </Row>
+      </Grid>
+        
+        <Table
+          list={list}
+          searchTerm={searchTerm}
+          removeItem={this.removeItem}
+        />
+
       </div>
     );
   }
 }
+
+class Search extends Component {
+  render(){
+    const { onChange, value, children } = this.props;
+    return(
+      <form>
+        <FormGroup>
+          <h1>{children}</h1>
+          <hr style={{border: '2px solid black', width: '100px'}} />
+          <div className="input-group">
+            <input
+              className="form-control width100 searchForm"
+              type="text"
+              onChange={onChange}
+              value={value}
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-primary searchBtn">Search</button>
+            </span>
+          </div>
+        </FormGroup>
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render(){
+    const { list, searchTerm, removeItem } = this.props;
+    return(
+      <div>
+        {
+          list.filter(isSearched(searchTerm)).map(item =>
+
+            <div key={item.objectId}>
+              <h1><a href={item.url}>{item.title}</a> by {item.author}</h1>
+              <h4>{item.comments} Comments | {item.points} Points</h4>
+              <Button
+                type="button"
+                onClick={ ()=>removeItem(item.objectId) }>
+                - Remove me -
+              </Button>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
+// class Button extends Component {
+//   render(){
+//     const { onClick, children } = this.props;
+//     return(
+//       <button onClick={onClick}>
+//         {children}
+//       </button>
+//     )
+//   }
+// }
+
+const Button = ({onClick, children}) =>
+  <button
+    onClick={onClick}>
+    {children}
+  </button>
 
 export default App;
---
+++ src/list.js
@@ -0,0 +1,28 @@
+const list = [
+    {
+        "author": "Ninja",
+        "title": "The hard earned black belt",
+        "url":"#",
+        "comments": 10,
+        "points": 2,
+        "objectId": 1
+    },
+    {
+        "author": "Madeira",
+        "title": "Oh no! The candy is out",
+        "url":"#",
+        "comments": 23,
+        "points": 45,
+        "objectId": 2
+    },
+    {
+        "author": "Canonial Germany",
+        "title": "The first time that i look for you, I am loved!",
+        "url":"#",
+        "comments": 12,
+        "points": 25,
+        "objectId": 3
+    }
+]
+
+export default list;
